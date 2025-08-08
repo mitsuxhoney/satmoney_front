0e4a870c8bdeca807a2bf54dd3d861bd52cab223
mitsuxhoney <hiveorxythm@gmail.com>
2025-08-08 15:34:12 +0530

    footer component changed

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./footer.css";
-
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/app/SATLogoFooter.png";
@@ -251,7 +250,7 @@ export const Footer = () => {
                                     <li>
                                         <Link
                                             className="flex items-center justify-center sm:justify-start gap-1.5 group text-white transition group-hover:text-white/75"
-                                            href="mailto:s  atsoftwareprivatelimited@gmail.com"
+                                            href="mailto:satsoftwareprivatelimited@gmail.com"
                                         >
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
@@ -259,11 +258,11 @@ export const Footer = () => {
                                                 fill="none"
                                                 viewBox="0 0 24 24"
                                                 stroke="currentColor"
-                                                stroke-width="2"
+                                                strokeWidth="2"
                                             >
                                                 <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
                                                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                                                 />
                                             </svg>
@@ -282,11 +281,11 @@ export const Footer = () => {
                                                 fill="none"
                                                 viewBox="0 0 24 24"
                                                 stroke="currentColor"
-                                                stroke-width="2"
+                                                strokeWidth="2"
                                             >
                                                 <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
                                                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                                                 />
                                             </svg>
@@ -306,16 +305,16 @@ export const Footer = () => {
                                                 fill="none"
                                                 viewBox="0 0 24 24"
                                                 stroke="currentColor"
-                                                stroke-width="2"
+                                                strokeWidth="2"
                                             >
                                                 <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
                                                     d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
                                                 />
                                                 <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
                                                     d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                                                 />
                                             </svg>
